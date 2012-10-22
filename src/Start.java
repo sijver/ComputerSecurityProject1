06708677f111be8cb318ddf715e9e64ce6b2b4f5
Mark <sijver@gmail.com>
2012-10-22 19:36:46 +0200

    init

---
+++ src/Start.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+
+/**
+ * @author Mark Skuratov, KhNURE.
+ * @version 1.0.
+ *          Date: 21.10.12.
+ */
+public class Start {
+
+    public static void main(String[] args) {
+
+        /*String[] plainTextsArray = StringGenerator.generatePlainTexts(1);*/
+        String key = StringGenerator.generateKey();
+        /*for(String s : plainTextsArray){
+            System.out.println(s);
+        }
+
+        byte[][] encryptedTexts = Encryptor.encryptNumberOfPlainTexts(plainTextsArray, key);
+
+        System.out.println();
+        for(byte[] b : encryptedTexts){
+            System.out.println(new String(b));
+        }
+        byte[][] decryptedTexts = Encryptor.decryptNumberOfCipherTexts(encryptedTexts, key);
+
+        System.out.println();
+        for(byte[] b : decryptedTexts){
+            System.out.println(new String(b));
+        } */
+
+        byte[][] texts = StringGenerator.generate256Texts();
+
+        byte[][] encryptedTexts = Encryptor.encryptNumberOfPlainTexts(texts, key);
+
+        byte[][] decryptedTexts = Encryptor.decryptNumberOfCipherTexts(encryptedTexts, key);
+
+        /*String plainText = "I Love Vika! I Love Vika!";
+        byte[] plaintTextBytes = plainText.getBytes();
+
+        String key = "78yz889923479122314312";
+        byte[] keyBytes = key.getBytes();
+
+        byte[] encryptedTextBytes = AES.encrypt(plaintTextBytes, keyBytes);
+        byte[] decryptedTextBytes = AES.decrypt(encryptedTextBytes, keyBytes);
+
+
+        System.out.println(plainText);
+        System.out.println(new String(encryptedTextBytes));
+        System.out.println(new String(decryptedTextBytes));*/
+
+    }
+
+}
---
+++ src/StringGenerator.java
@@ -0,0 +1,49 @@
+import java.util.Random;
+
+/**
+ * @author Mark Skuratov, KhNURE.
+ * @version 1.0.
+ *          Date: 21.10.12.
+ */
+public class StringGenerator {
+
+    public static String[] generatePlainTexts(int plainTextsNumber) {
+        String[] plainTextsArray = new String[plainTextsNumber];
+        for (int i = 0; i < plainTextsNumber; i++) {
+            plainTextsArray[i] = generateString();
+        }
+        return plainTextsArray;
+    }
+
+    public static String generateKey(){
+        return generateString();
+    }
+
+    private static String generateString() {
+        final int STRING_LENGTH = 16;
+        final String chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_ []{}|;':,./<>?\\\"`~";
+        final Random rnd = new Random();
+        char[] text = new char[STRING_LENGTH];
+        for (int i = 0; i < STRING_LENGTH; i++) {
+            text[i] = chars.charAt(rnd.nextInt(chars.length()));
+        }
+        return new String(text);
+    }
+
+    public static byte[][] generate256Texts(){
+        Random random = new Random();
+        byte[][] plainTexts = new byte[256][];
+        plainTexts[0] = new byte[16];
+        plainTexts[0][0] = 0;
+        for(int i = 1; i < 16; i++){
+            plainTexts[0][i] = (byte)random.nextInt(256);
+        }
+        for(int i = 1; i < 256; i++){
+            plainTexts[i] = new byte[16];
+            plainTexts[i] = plainTexts[0].clone();
+            plainTexts[i][0] = (byte)i;
+        }
+        return plainTexts;
+    }
+
+}
