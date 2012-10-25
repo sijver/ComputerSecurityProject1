db0a94bb0e9d2c1f32834854445601b98887b5e1
Mark <sijver@gmail.com>
2012-10-25 16:20:27 +0200

    added swing interface. fixed bugs

--- src/Start.java
+++ src/Start.java
@@ -9,21 +9,6 @@ public class Start {
 
     public static void main(String[] args) {
 
-        //This code will be deleted when the GUI will be made
-        /*SquareAttack.generateKey();
-
-        for (int i = 0; i < 16; i++) {
-            byte[][] texts = SquareAttack.generate256Texts(i);
-
-            byte[][] enc = SquareAttack.encryptTexts(texts);
-
-            byte rec = SquareAttack.attackKeyByte(enc, i);
-
-            System.out.println(rec);
-        }
-
-        System.out.println(Arrays.deepToString(AES.getRoundKey(4))); */
-
         JFrame mainFrame = new MainFrame();
 
     }
